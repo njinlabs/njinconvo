45ed265f4b0922da70faf7d683bf68edc1e30153
njintechcom <services@njintech.com>
2023-09-06 15:48:39 +0700

    add: update meeting link

--- app/Controllers/Http/MeetingsController.ts
+++ app/Controllers/Http/MeetingsController.ts
@@ -1,6 +1,6 @@
 import { AuthContract } from '@ioc:Adonis/Addons/Auth'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import { schema, rules } from '@ioc:Adonis/Core/Validator'
+import { rules, schema } from '@ioc:Adonis/Core/Validator'
 import Database from '@ioc:Adonis/Lucid/Database'
 import { ManyToManyQueryBuilderContract, ModelQueryBuilderContract } from '@ioc:Adonis/Lucid/Orm'
 import Classroom from 'App/Models/Classroom'
@@ -84,17 +84,25 @@ export default class MeetingsController {
       }),
     })
 
-    return await Database.transaction(async () => {
-      const meeting = await Meeting.create({
-        title,
-        description,
-        isDraft,
-      })
+    return await Database.transaction(async (trx) => {
+      const meeting = await Meeting.create(
+        {
+          title,
+          description,
+          isDraft,
+        },
+        {
+          client: trx,
+        }
+      )
 
       let meetingLinks: MeetingLink[] = []
       if (links?.length) {
         meetingLinks = await MeetingLink.createMany(
-          links.map((link) => ({ ...link, meetingId: meeting.id }))
+          links.map((link) => ({ ...link, meetingId: meeting.id })),
+          {
+            client: trx,
+          }
         )
       }
 
@@ -125,32 +133,72 @@ export default class MeetingsController {
   }
 
   public async update({ auth, params, request }: HttpContextContract) {
-    const classroom = await this.getClassroom(auth, params.classroomId, true)
-    const meeting = await classroom
-      .related('meetings')
-      .query()
-      .where('meetings.id', params.id)
-      .firstOrFail()
+    return await Database.transaction(async (trx) => {
+      const classroom = await this.getClassroom(auth, params.classroomId, true)
+      const meeting = await classroom
+        .related('meetings')
+        .query()
+        .useTransaction(trx)
+        .where('meetings.id', params.id)
+        .preload('links')
+        .firstOrFail()
 
-    const {
-      title,
-      description,
-      is_draft: isDraft,
-    } = await request.validate({
-      schema: schema.create({
-        title: schema.string(),
-        description: schema.string(),
-        is_draft: schema.boolean(),
-      }),
+      const {
+        title,
+        description,
+        is_draft: isDraft,
+        links,
+      } = await request.validate({
+        schema: schema.create({
+          title: schema.string(),
+          description: schema.string(),
+          is_draft: schema.boolean(),
+          links: schema.array.optional().members(
+            schema.object().members({
+              id: schema.number.nullableAndOptional(),
+              title: schema.string(),
+              url: schema.string({}, [rules.url()]),
+            })
+          ),
+        }),
+      })
+
+      meeting.title = title
+      meeting.description = description
+      meeting.isDraft = isDraft
+
+      await meeting.useTransaction(trx).save()
+
+      const destroyLinks = meeting.links.filter(
+        (item) => !links?.find((el) => `${el.id}` === `${item.id}`)
+      )
+      const addLinks = links?.filter((el) => !el.id)
+      const setLinks = meeting.links.filter(
+        (item) => links?.find((el) => `${el.id}` === `${item.id}`)
+      )
+
+      for (const link of destroyLinks) {
+        await link.useTransaction(trx).delete()
+      }
+
+      for (const link of setLinks) {
+        const data = links?.find((el) => `${el.id}` === `${link.id}`)!
+        await link.useTransaction(trx).merge({ title: data.title, url: data.url }).save()
+      }
+
+      if (addLinks?.length) {
+        await MeetingLink.createMany(
+          addLinks?.map((link) => ({ title: link.title, url: link.url, meetingId: meeting.id })),
+          {
+            client: trx,
+          }
+        )
+      }
+
+      await meeting.load('links')
+
+      return { ...meeting.serialize() }
     })
-
-    meeting.title = title
-    meeting.description = description
-    meeting.isDraft = isDraft
-
-    await meeting.save()
-
-    return meeting.serialize()
   }
 
   public async destroy({ auth, params }: HttpContextContract) {
