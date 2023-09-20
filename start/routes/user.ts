0295ab9cb55a8516bb5dc232d5f0e3a3dfcdb962
akbaraditamasp <akbaraditamasp@gmail.com>
2023-09-20 10:22:51 +0700

    fix: remove private middleware

--- start/routes/user.ts
+++ start/routes/user.ts
@@ -1,10 +1,7 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
-  Route.put('/profile', 'UsersController.updateProfile').middleware([
-    'auth:user',
-    'private:lead,participant',
-  ])
+  Route.put('/profile', 'UsersController.updateProfile').middleware(['auth:user'])
   Route.delete('/:id', 'UsersController.destroy').middleware(['auth:user', 'private:administrator'])
   Route.get('/:id', 'UsersController.show').middleware(['auth:user', 'private:administrator'])
   Route.put('/:id', 'UsersController.update').middleware(['auth:user', 'private:administrator'])
